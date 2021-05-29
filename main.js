da567b08c5153840535d19902b7b7221b00bc0d5
Pratham443 <77573159+Pratham443@users.noreply.github.com>
2021-05-29 16:00:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,3 +18,27 @@ function take_snapshot() {
 console.log("ml5 version :", ml5.version);
 
 classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/gBAcvB7D2/model.json", modelLoaded);
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!")
+}
+
+function check() {
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    else{
+        console.log(results)
+        document.getElementById("object_name").innerHTML = results[0].label;
+        document.getElementById("object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
